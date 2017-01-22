216b6d0c25a371f6b322d8ac5c50b3c6d8779b71
Tony Lam <supuilam@gmail.com>
2017-01-21 22:26:34 -0500

    Articles model (#1)
    * Set up articles table to have title and body
    
    * Create articles#index page
    
    * Create articles#index page
    
    * Create articles/new routes and make link to articles functional
    
    * Add style to App
    
    * Complete create new articles feature with error handling
    
    * Complete delete button function in articles#show with confirmation
    
    * Complete update function with error handling
    
    * Complete Articles feature

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  include ArticlesHelper
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash.notice = "Article '#{@article.title}' Created!"
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id]).destroy
+    flash.notice = "Article '#{@article.title}' Deleted."
+    redirect_to '/articles'
+  end
+end
