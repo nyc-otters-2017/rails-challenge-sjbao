e2bec0e1a127e118cfb22e9d5b285f14a241025d
sjBao <supuilam@gmail.com>
2017-01-21 20:43:03 -0500

    Create articles/new routes and make link to articles functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'new_article'
   get '/articles/:id' => 'articles#show', as: 'article'
 end
