efbdf697af214de4df9667d269d3fa6fead78480
sjBao <supuilam@gmail.com>
2017-01-21 21:14:14 -0500

    Complete create new articles feature with error handling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,23 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new', as: 'new_article'
+  post '/articles' => 'articles#create'
   get '/articles/:id' => 'articles#show', as: 'article'
 end
