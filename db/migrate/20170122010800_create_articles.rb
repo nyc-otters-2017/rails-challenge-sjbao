43f1e498794fbd4d4b1d31fe49c2f0ef22416638
sjBao <supuilam@gmail.com>
2017-01-21 20:12:25 -0500

    Set up articles table to have title and body

---
+++ db/migrate/20170122010800_create_articles.rb
@@ -0,0 +1,9 @@
+class CreateArticles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :articles do |t|
+      t.string :title, :null => false
+      t.text :body, :null => false
+      t.timestamps
+    end
+  end
+end
