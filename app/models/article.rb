70632409c7f4eb9acaa5bd8a0725d581bbd3b52b
Tony Lam <email": null>
2017-01-23 14:35:29 -0500

    Add image feature to article or blog entries

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => [
+    "image/jpg", "image/jpeg", "image/png"]
   validates :title, :body, { presence: true }
   has_many :comments
   has_many :taggings
