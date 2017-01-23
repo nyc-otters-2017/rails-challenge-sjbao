b75a3be781ff4ed69190dc2de3f8251d8476986f
Tony Lam <email": null>
2017-01-23 18:18:15 -0500

    Did a lot of stuff, user auth, protected routes, etc, this is bad git workflow sorry

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
+  root to: 'articles#index'
+
   resources :articles do
     resources :comments
   end
+
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 
   get '/tags' => 'tags#index', as: 'tags'
   get '/tags/:id' => 'tags#show', as: 'tag'
